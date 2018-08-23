004822adc61ac689ddbbf4c57e3e65102a390947
Ever <439674061@qq.com>
2018-08-23 17:19:02 +0800

    update .gitignore

--- src/main/java/com/github/dapeng/NettyHttpServer.java
+++ src/main/java/com/github/dapeng/NettyHttpServer.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
  * @since 2018年08月23日 上午9:54
  */
 public class NettyHttpServer {
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private static final Logger logger = LoggerFactory.getLogger(NettyHttpServer.class);
 
     private final int port;
 
