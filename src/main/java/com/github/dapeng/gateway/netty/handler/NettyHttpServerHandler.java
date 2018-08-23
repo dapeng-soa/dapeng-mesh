d433207b1e1b81d83e9a9a977c63e46898b5b641
leihuazhe <295482300@qq.com>
2018-08-24 00:27:35 +0800

    add keeper alive

--- src/main/java/com/github/dapeng/gateway/netty/handler/NettyHttpServerHandler.java
+++ src/main/java/com/github/dapeng/gateway/netty/handler/NettyHttpServerHandler.java
@@ -17,7 +17,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -28,9 +27,11 @@ import java.util.regex.Pattern;
  */
 public class NettyHttpServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private static Logger logger = LoggerFactory.getLogger(NettyHttpServerHandler.class);
+
     private PathMatcher pathMatcher = new AntPathMatcher();
-//    private final String DEFAULT_MATCH = "/api/{serviceName:[\\s\\S]*}/{version:[\\s\\S]*}/{methodName:[\\s\\S]*}";
+    private final String DEFAULT_MATCH = "/api/{serviceName:[\\s\\S]*}/{version:[\\s\\S]*}/{methodName:[\\s\\S]*}";
+
     private final String DEFAULT_MATCH_AUTH = "/api/{serviceName:[\\s\\S]*}/{version:[\\s\\S]*}/{methodName:[\\s\\S]*}/{apiKey:[\\s\\S]*}";
 
     private final Pattern pathPattern = Pattern.compile("([\\s\\S]*)\\?([\\s\\S]*)");
@@ -41,7 +42,7 @@ public class NettyHttpServerHandler extends SimpleChannelInboundHandler<FullHttp
         try {
             doService(httpRequest, ctx);
         } catch (Exception e) {
-            sendHttpResponse(ctx, "处理请求失败!", HttpResponseStatus.INTERNAL_SERVER_ERROR);
+            sendHttpResponse(ctx, "处理请求失败!", null, HttpResponseStatus.INTERNAL_SERVER_ERROR);
             logger.error("处理请求失败!" + e.getMessage(), e);
         }
     }
@@ -63,17 +64,12 @@ public class NettyHttpServerHandler extends SimpleChannelInboundHandler<FullHttp
             handlerPostRequest(request, ctx);
             return;
         }
-        sendHttpResponse(ctx, "不符合的请求", HttpResponseStatus.OK);
+        sendHttpResponse(ctx, "不符合的请求", request, HttpResponseStatus.OK);
     }
 
     private void handlerPostRequest(FullHttpRequest request, ChannelHandlerContext ctx) {
         String uri = request.uri();
-       /* Matcher matcher = pathPattern.matcher(uri);
-        if (matcher.matches()) {
-            logger.info(matcher.group(0));
-            logger.info(matcher.group(1));
-            logger.info(matcher.group(2));
-        }*/
+
 
         if (pathMatcher.match(DEFAULT_MATCH_AUTH, uri)) {
             Map<String, String> pathVariableMap = pathMatcher.extractUriTemplateVariables(DEFAULT_MATCH_AUTH, request.uri());
@@ -88,72 +84,67 @@ public class NettyHttpServerHandler extends SimpleChannelInboundHandler<FullHttp
             jsonResponse.whenComplete((result, ex) -> {
                 if (ex != null) {
                     String resp = String.format("{\"responseCode\":\"%s\", \"responseMsg\":\"%s\", \"success\":\"%s\", \"status\":0}", SoaCode.ServerUnKnown.getCode(), ex.getMessage(), "{}");
-                    sendHttpResponse(ctx, resp, HttpResponseStatus.OK);
+                    sendHttpResponse(ctx, resp, request, HttpResponseStatus.OK);
                 } else {
                     if (result.contains("status")) {
-                        sendHttpResponse(ctx, result, HttpResponseStatus.OK);
+                        sendHttpResponse(ctx, result, request, HttpResponseStatus.OK);
                         return;
                     }
                     String response = "{}".equals(result) ? "{\"status\":1}" : result.substring(0, result.lastIndexOf('}')) + ",\"status\":1}";
-                    sendHttpResponse(ctx, response, HttpResponseStatus.OK);
+                    sendHttpResponse(ctx, response, request, HttpResponseStatus.OK);
                 }
             });
         } else {
-            sendHttpResponse(ctx, "不合法的请求", HttpResponseStatus.OK);
+            sendHttpResponse(ctx, "不合法的请求", request, HttpResponseStatus.OK);
         }
     }
-
 
     private void handlerGetAndHead(FullHttpRequest request, ChannelHandlerContext ctx) {
         String uri = request.uri();
         if ("/health/check".equals(uri)) {
             logger.debug("health check,container status: " + ContainerStatus.GREEN);
-            sendHttpResponse(ctx, "GateWay is running", HttpResponseStatus.OK);
+            sendHttpResponse(ctx, "GateWay is running", request, HttpResponseStatus.OK);
         } else {
             logger.debug("not support url request, uri: {}", uri);
-            sendHttpResponse(ctx, "不支持的请求类型", HttpResponseStatus.OK);
+            sendHttpResponse(ctx, "不支持的请求类型", request, HttpResponseStatus.OK);
         }
     }
-
-    /*private String getPathWithArgsUri(String uri) {
-        String path;
-        Map<String, String> args;
-        Matcher matcher = pathPattern.matcher(uri);
-        if (matcher.matches()) {
-            path = matcher.group(1);
-            String strArgs = matcher.group(2);
-            Arrays.stream(strArgs.split("&")).map(kv-> {
-                kv.split("=")
-
-            )
-        }
-
-        return "";
-    }*/
 
     /**
      * 返回信息给前端 http
      *
      * @param ctx     handler's context
      * @param content msg's info
+     * @param request msg's request
      * @param status  http status
      */
-    private void sendHttpResponse(ChannelHandlerContext ctx, String content, HttpResponseStatus status) {
+    private void sendHttpResponse(ChannelHandlerContext ctx, String content, FullHttpRequest request, HttpResponseStatus status) {
         FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status, Unpooled.copiedBuffer(content, CharsetUtil.UTF_8));
         response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
-        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+
+        if (request == null) {
+            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+        } else {
+            boolean isKeepAlive = HttpUtil.isKeepAlive(request);
+            if (isKeepAlive) {
+                response.headers().set(HttpHeaderNames.CONNECTION,
+                        HttpHeaderValues.KEEP_ALIVE);
+                ctx.writeAndFlush(response);
+            } else {
+                ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+            }
+        }
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         logger.info("连接的客户端地址:{}", ctx.channel().remoteAddress());
-//        ctx.writeAndFlush("客户端" + InetAddress.getLocalHost().getHostName() + "成功与服务端建立连接！ ");
         super.channelActive(ctx);
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         logger.error(cause.getMessage(), cause);
-        sendHttpResponse(ctx, "服务器错误", HttpResponseStatus.INTERNAL_SERVER_ERROR);
+        sendHttpResponse(ctx, "服务器错误: " + cause.getMessage(), null, HttpResponseStatus.INTERNAL_SERVER_ERROR);
     }
 }
