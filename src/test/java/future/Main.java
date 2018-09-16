9037261a7d7ef398eb7db267a4b5d3b6aacf73c8
leihuazhe <295482300@qq.com>
2018-09-16 22:31:13 +0800

    add test

--- src/test/java/future/Main.java
+++ src/test/java/future/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
         Ctest2 test = new Ctest2();
 
 
